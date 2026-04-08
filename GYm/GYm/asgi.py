a5fafb04d3422a1efeb7356851a2ef998cfe3c5c
anjansai14 <Anjansai118@gmail.com>
2026-04-08 15:57:36 +0530

    total changes

--- GYm/GYm/asgi.py
+++ GYm/GYm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GYm project.
+ASGI config for GYM project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYM.settings')
 
 application = get_asgi_application()
--- GYm/manage.py
+++ GYm/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYm.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYM.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
