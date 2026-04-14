f799bb082fe2d9c32ab314ac2fa58a39be9ca912
anjansai14 <Anjansai118@gmail.com>
2026-04-14 11:09:22 +0530

    GYmtoGYM

--- GYm/GYm/asgi.py
+++ GYm/GYm/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GYM.settings')
 
 application = get_asgi_application()
